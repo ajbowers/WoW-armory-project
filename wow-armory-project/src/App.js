b68224a29f4521119241b48c4c9942d050437f43
ajbowers <d.bowers90@gmail.com>
2017-06-17 13:22:43 -0400

    folder structure

--- wow-armory-project/src/App.js
+++ wow-armory-project/src/App.js
@@ -8,10 +8,7 @@ class App extends Component {
       <div className="Body">
         <Navbar />
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
+          
           <p className="App-intro">
             To get started, test test edit <code>src/App.js</code> and save to reload.
           </p>
--- wow-armory-project/src/navbar/Navbar.js
+++ wow-armory-project/src/navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 class Navbar extends Component {
   render() {
     return (
-      <div className="Navbar">
+      <div className="navbar-container">
         <div className="navbar-dropdown"> 
             <p> Home </p>
         </div>
